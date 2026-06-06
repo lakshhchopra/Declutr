47f2d3ca900bc2317d20520b2d51026c77636df2
Maithili <pawarmd37@gmail.com>
2026-06-06 21:56:03 +0530

    Define SRP login protocol models

---
+++ backend/internal/auth/models/login.go
@@ -0,0 +1,21 @@
+package models
+
+type LoginStartRequest struct {
+        Email string `json:"email"`
+}
+
+type LoginStartResponse struct {
+        Salt string `json:"salt"`
+        ServerPublicKey string `json:"serverPublicKey"`
+}
+
+type LoginFinishRequest struct {
+        Email string `json:"email"`
+        ClientPublicKey string `json:"clientPublicKey"`
+        ClientProof string `json:"clientProof"`
+}
+
+type LoginFinishResponse struct {
+        ServerProof string `json:"serverProof"`
+        AccessToken string `json:"accessToken"`
+}
